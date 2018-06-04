12bb973c224b29fc8702b7c2c1e7f0dddcff5f2d
YuhangSong <YuhangSong2017@gmail.com>
2018-06-04 17:01:34 +0800

    stochasticity comes from reset_game, sad!

--- stochasticity_tree.py
+++ stochasticity_tree.py
@@ -4,12 +4,21 @@ from ale_python_interface import ALEInterface
 import copy
 
 # test = 'loadROM'
-# test = 'restoreState'
-test = 'restoreSystemState'
+test = 'restoreState'
+# test = 'restoreSystemState'
 
 frame_skip = 4
-bunch = 200
+bunch = 100
 sequence = 800
+
+spaces = '                                                     '
+
+def clear_print_line():
+    print(spaces,end="\r")
+
+def clear_print(string_to_print):
+    clear_print_line()
+    print(string_to_print,end="\r")
 
 def main():
     result = {
@@ -42,16 +51,16 @@ def main():
         game_path = atari_py.get_game_path(game)
         game_path = str.encode(game_path)
 
-        env_father = ALEInterface()
-        env_father.setFloat('repeat_action_probability'.encode('utf-8'), 0.0)
-        env_father.setInt(b'random_seed', 3)
-        env_father.loadROM(game_path)
-        env_father.reset_game()
+        env = ALEInterface()
+        env.setFloat('repeat_action_probability'.encode('utf-8'), 0.0)
+        env.setInt(b'random_seed', 3)
+        env.loadROM(game_path)
+        env.reset_game()
 
         if test in ['restoreState']:
-            state_after_reset = env_father.cloneState()
+            state_after_reset = env.cloneState()
         if test in ['restoreSystemState']:
-            state_after_reset = env_father.cloneSystemState()
+            state_after_reset = env.cloneSystemState()
 
         print('=====================================================')
         try:
@@ -65,7 +74,7 @@ def main():
         except Exception as e:
             '''generate a sequence of actions'''
             action_sequence = np.random.randint(
-                len(env_father.getMinimalActionSet()),
+                len(env.getMinimalActionSet()),
                 size = sequence,
             )
             np.save(
@@ -80,32 +89,53 @@ def main():
 
         bunch_obs = []
         distribution = []
-        samples = []
+        state_metrix = []
         for bunch_i in range(bunch):
 
-            env_temp = env_father
-            env_temp.setInt(b'random_seed', 3)
             if test in ['loadROM']:
-                env_temp.loadROM(game_path)
-                env_temp.reset_game()
+                env.setInt(b'random_seed', 3)
+                env.loadROM(game_path)
+                env.reset_game()
             elif test in ['restoreState']:
-                env_temp.restoreState(state_after_reset)
+                env.restoreState(state_after_reset)
             elif test in ['restoreSystemState']:
-                env_temp.restoreSystemState(state_after_reset)
+                env.restoreSystemState(state_after_reset)
 
+            state_sequence = [env.cloneState()]
+
+            has_terminated = False
             for sequence_i in range(sequence):
+                # for frame_skip_i in range(frame_skip):
+                #     if not has_terminated:
+                #         env.act(
+                #             env.getMinimalActionSet()[
+                #                 action_sequence[sequence_i]
+                #             ]
+                #         )
+                #         if env.game_over():
+                #             has_terminated = True
                 for frame_skip_i in range(frame_skip):
-                    env_temp.act(
-                        env_father.getMinimalActionSet()[
-                            action_sequence[sequence_i]
-                        ]
-                    )
-                if env_temp.game_over():
-                    env_temp.reset_game()
+                    if not env.game_over():
+                        env.act(
+                            env.getMinimalActionSet()[
+                                action_sequence[sequence_i]
+                            ]
+                        )
+                        if env.game_over():
+                            episode_length = sequence_i
 
-            obs = env_temp.getScreenRGB()
+                try:
+                    clear_print('[{}|{}|{}]'.format(bunch_i,sequence_i,episode_length))
+                except Exception as e:
+                    pass
 
-            samples += [obs]
+                if has_terminated:
+                    state_sequence += [-1]
+                else:
+                    state_sequence += [env.cloneState()]
+
+            obs = env.getScreenRGB()
+
             found_at_bunch = -1
             if_has_identical_one = False
             max_value = 0
